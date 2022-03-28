07288be7bf95bb81fc3409262c1f66c3808009fe
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2022-03-28 05:30:08 +0300

    multicall address and token list

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -12,7 +12,7 @@ export default {
     56: '0x5aF6D33DE2ccEC94efb1bDF8f92Bd58085432d2c',
   },
   multiCall: {
-    56: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B',
+    25: '0xd5eC889d337Da77a3a537b65B281ed21E5C23563',
     97: '0x8F3273Fb89B075b1645095ABaC6ed17B2d4Bc576',
   },
   pancakeProfile: {
