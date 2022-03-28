90fa30edf0682ff5e31bc610b0c531236d255517
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2022-03-28 05:10:37 +0300

    Initial commit

---
+++ packages/uikit/src/components/Image/tokens.ts
@@ -0,0 +1,88 @@
+/**
+ * @see https://github.com/pancakeswap/pancake-frontend/blob/develop/src/config/constants/tokens.ts
+ */
+const tokens = {
+  cake: {
+    symbol: "CCS",
+    address: {
+      25: "0xA70caD8524fEBC2162D2246355Cae1bC48E3679C",
+      97: "0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe",
+    },
+    decimals: 18,
+    projectLink: "https://pancakeswap.finance/",
+  },
+  usdc: {
+    symbol: "USDC",
+    address: {
+      25: "0xc21223249CA28397B4B6541dfFaEcC539BfF0c59",
+      97: "",
+    },
+    decimals: 18,
+    projectLink: "https://www.centre.io/usdc",
+  },
+  wbnb: {
+    symbol: "WCRO",
+    address: {
+      25: "0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23",
+      97: "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",
+    },
+    decimals: 18,
+    projectLink: "https://pancakeswap.finance/",
+  },
+  busd: {
+    symbol: "BUSD",
+    address: {
+      25: "0x6aB6d61428fde76768D7b45D8BFeec19c6eF91A8",
+      97: "",
+    },
+    decimals: 18,
+    projectLink: "https://www.paxos.com/busd/",
+  },
+  eth: {
+    symbol: "WETH",
+    address: {
+      25: "0xe44Fd7fCb2b1581822D0c862B68222998a0c299a",
+      97: "",
+    },
+    decimals: 18,
+    projectLink: "https://ethereum.org/en/",
+  },
+  wcro: {
+    symbol: "WCRO",
+    address: {
+      25: "0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23",
+      97: "",
+    },
+    decimals: 18,
+    projectLink: "https://ethereum.org/en/",
+  },
+  usdt: {
+    symbol: "USDT",
+    address: {
+      25: "0x66e428c3f67a68878562e79A0234c1F83c208770",
+      97: "0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5",
+    },
+    decimals: 18,
+    projectLink: "https://tether.to/",
+  },
+  corna: {
+    symbol: "CORNA",
+    address: {
+      25: "0xadbd1231fb360047525BEdF962581F3eee7b49fe",
+      97: "0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5",
+    },
+    decimals: 18,
+    projectLink: "https://bitcoin.org/",
+  },
+  wbtc: {
+    symbol: "WBTC",
+    address: {
+      25: "0x062E66477Faf219F25D27dCED647BF57C3107d52",
+      97: "0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5",
+    },
+    decimals: 18,
+    projectLink: "https://bitcoin.org/",
+  },
+ };
+
+export default tokens;
